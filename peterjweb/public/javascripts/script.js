ceecdba6235746ffae711aa1acb6f15b77aea01c
Peter Jacobs <thisispeterj@gmail.com>
2017-04-13 10:01:10 -0700

    Complete pug paritals, add CSS styling, javascriptfunctionality

---
+++ peterjweb/public/javascripts/script.js
@@ -0,0 +1,38 @@
+var dateTime = new Date();
+var myAge =  new Date().getFullYear() - 1985;
+var residence = new Date().getFullYear() - 1999;
+
+$('.residence').text(residence);
+$('.myAge').text(myAge);
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+	var width;
+	//Portfolio Section Animations
+	if (wScroll > $('#portfolio').offset().top) {
+		$('#projects .project').each(function(i) {
+			setTimeout(function() {
+				$('.project').eq(i).addClass('show-elements');	
+			}, 250 * (i+1));
+		});
+	};
+
+	//Skill Bar Animations
+	if (wScroll > $('#skills').offset().top) {
+		$("#skills .skills-chart .skill span").each(function() {
+			var dataWidth = $(this).data("width") + "%";			
+			var styles = {
+				width : dataWidth,
+				opacity : 1
+			}
+			$(this).css(styles);		
+		});	
+	};
+
+	//Footer Reveal 
+	// if (wScroll > $('#contact').offset().top) {
+	// 	$(".nav-position").removeClass("nav-position-full").addClass("nav-position-mini");
+	// } else {
+	// 	$(".nav-position").removeClass("nav-position-mini").addClass("nav-position-full");
+	// }
+});
